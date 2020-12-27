773063595118f454ba6710f00f9311ed471a33f5
Sotirios Mantziaris <smantziaris@gmail.com>
2020-12-27 15:10:17 +0200

    SQS v2 client and example update

--- test/docker/kafka/client_integration_test.go
+++ test/docker/kafka/client_integration_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/Shopify/sarama"
+	v1 "github.com/beatlabs/patron/client/kafka"
 	v2 "github.com/beatlabs/patron/client/kafka/v2"
 	opentracing "github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/ext"
@@ -19,20 +20,33 @@ const (
 	clientTopic = "clientTopic"
 )
 
-func TestNewAsyncProducer_Success(t *testing.T) {
+func TestNewAsyncProducer_Success_v2(t *testing.T) {
 	ap, chErr, err := v2.New(Brokers()).CreateAsync()
 	assert.NoError(t, err)
 	assert.NotNil(t, ap)
 	assert.NotNil(t, chErr)
 }
 
-func TestNewSyncProducer_Success(t *testing.T) {
+func TestNewAsyncProducer_Success_v1(t *testing.T) {
+	ap, chErr, err := v1.NewBuilder(Brokers()).CreateAsync()
+	assert.NoError(t, err)
+	assert.NotNil(t, ap)
+	assert.NotNil(t, chErr)
+}
+
+func TestNewSyncProducer_Success_v2(t *testing.T) {
 	p, err := v2.New(Brokers()).Create()
 	assert.NoError(t, err)
 	assert.NotNil(t, p)
 }
 
-func TestAsyncProducer_SendMessage_Close(t *testing.T) {
+func TestNewSyncProducer_Success_v1(t *testing.T) {
+	p, err := v1.NewBuilder(Brokers()).CreateSync()
+	assert.NoError(t, err)
+	assert.NotNil(t, p)
+}
+
+func TestAsyncProducer_SendMessage_Close_v2(t *testing.T) {
 	mtr := mocktracer.New()
 	defer mtr.Reset()
 	opentracing.SetGlobalTracer(mtr)
@@ -60,7 +74,32 @@ func TestAsyncProducer_SendMessage_Close(t *testing.T) {
 	assert.Equal(t, expected, mtr.FinishedSpans()[0].Tags())
 }
 
-func TestSyncProducer_SendMessage_Close(t *testing.T) {
+func TestAsyncProducer_SendMessage_Close_v1(t *testing.T) {
+	mtr := mocktracer.New()
+	defer mtr.Reset()
+	opentracing.SetGlobalTracer(mtr)
+	ap, chErr, err := v1.NewBuilder(Brokers()).CreateAsync()
+	assert.NoError(t, err)
+	assert.NotNil(t, ap)
+	assert.NotNil(t, chErr)
+	msg := v1.NewMessage(clientTopic, "TEST")
+	err = ap.Send(context.Background(), msg)
+	assert.NoError(t, err)
+	assert.NoError(t, ap.Close())
+	assert.Len(t, mtr.FinishedSpans(), 1)
+
+	expected := map[string]interface{}{
+		"component": "kafka-async-producer",
+		"error":     false,
+		"span.kind": ext.SpanKindEnum("producer"),
+		"topic":     clientTopic,
+		"type":      "async",
+		"version":   "dev",
+	}
+	assert.Equal(t, expected, mtr.FinishedSpans()[0].Tags())
+}
+
+func TestSyncProducer_SendMessage_Close_v2(t *testing.T) {
 	mtr := mocktracer.New()
 	defer mtr.Reset()
 	opentracing.SetGlobalTracer(mtr)
@@ -89,7 +128,31 @@ func TestSyncProducer_SendMessage_Close(t *testing.T) {
 	assert.Equal(t, expected, mtr.FinishedSpans()[0].Tags())
 }
 
-func TestAsyncProducerActiveBrokers(t *testing.T) {
+func TestSyncProducer_SendMessage_Close_v1(t *testing.T) {
+	mtr := mocktracer.New()
+	defer mtr.Reset()
+	opentracing.SetGlobalTracer(mtr)
+	p, err := v1.NewBuilder(Brokers()).CreateSync()
+	require.NoError(t, err)
+	assert.NotNil(t, p)
+	msg := v1.NewMessage(clientTopic, "TEST")
+	err = p.Send(context.Background(), msg)
+	assert.NoError(t, err)
+	assert.NoError(t, p.Close())
+	assert.Len(t, mtr.FinishedSpans(), 1)
+
+	expected := map[string]interface{}{
+		"component": "kafka-sync-producer",
+		"error":     false,
+		"span.kind": ext.SpanKindEnum("producer"),
+		"topic":     clientTopic,
+		"type":      "sync",
+		"version":   "dev",
+	}
+	assert.Equal(t, expected, mtr.FinishedSpans()[0].Tags())
+}
+
+func TestAsyncProducerActiveBrokers_v2(t *testing.T) {
 	ap, chErr, err := v2.New(Brokers()).CreateAsync()
 	assert.NoError(t, err)
 	assert.NotNil(t, ap)
@@ -98,8 +161,25 @@ func TestAsyncProducerActiveBrokers(t *testing.T) {
 	assert.NoError(t, ap.Close())
 }
 
-func TestSyncProducerActiveBrokers(t *testing.T) {
+func TestAsyncProducerActiveBrokers_v1(t *testing.T) {
+	ap, chErr, err := v1.NewBuilder(Brokers()).CreateAsync()
+	assert.NoError(t, err)
+	assert.NotNil(t, ap)
+	assert.NotNil(t, chErr)
+	assert.NotEmpty(t, ap.ActiveBrokers())
+	assert.NoError(t, ap.Close())
+}
+
+func TestSyncProducerActiveBrokers_v2(t *testing.T) {
 	ap, err := v2.New(Brokers()).Create()
+	assert.NoError(t, err)
+	assert.NotNil(t, ap)
+	assert.NotEmpty(t, ap.ActiveBrokers())
+	assert.NoError(t, ap.Close())
+}
+
+func TestSyncProducerActiveBrokers_v1(t *testing.T) {
+	ap, err := v1.NewBuilder(Brokers()).CreateSync()
 	assert.NoError(t, err)
 	assert.NotNil(t, ap)
 	assert.NotEmpty(t, ap.ActiveBrokers())
